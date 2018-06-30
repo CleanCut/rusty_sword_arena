c299fc4b1e7f7e6a37eb595cd7ef9c5d31df9a0f
Nathan Stocks <cleancut@github.com>
2018-06-30 16:28:22 -0600

    Add a script to watch and rebuild stuff on the web server.

---
+++ rebuild.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+WATCHFILE="/tmp/rusty_sword_arena.rebuild"
+DOCS_DIR="/web/static/rusty_file_arena"
+
+# For when things go horribly wrong
+function die {
+    echo
+    echo $1
+    echo
+    exit 2
+}
+
+while true ; do
+    if [ -e ${WATCHFILE} ] ; then
+        # Cleanup watchfile
+        rm ${WATCHFILE}
+        # Pull new source code
+        git pull origin master || die "Error pulling from GitHub"
+        # Build the docs
+        rm -rf ${DOCS_DIR} || die "Failed cleaning previous docs"
+        cargo doc --lib --no-deps --target-dir ${TARGET} || die "Failed generating documentation"
+        # Build new server binary
+        cargo build --release --bin server || die "Failed building the server"
+        # Restart server
+        systemctl restart rusty_sword_arena || die "Failed restarting the server"
+    fi
+    sleep 1;
+done
+
